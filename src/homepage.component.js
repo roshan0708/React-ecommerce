1b2bbd56ab7316e3efee8fdbf1b816e2976191c6
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-22 18:49:31 +0530

    first commit and added files of homepage

---
+++ src/homepage.component.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+    return (
+            <div className="homepage">
+                <div className="directory-menu">
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">HATS</h1>
+                            <span className="subtitle">SHOP NOW</span>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">JACKETS</h1>
+                            <span className="subtitle">SHOP NOW</span>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">SNEAKERS</h1>
+                            <span className="subtitle">SHOP NOW</span>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">WOMENS</h1>
+                            <span className="subtitle">SHOP NOW</span>
+                        </div>
+                    </div>
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1 className="title">MENS</h1>
+                            <span className="subtitle">SHOP NOW</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+}
+
+export default HomePage;
