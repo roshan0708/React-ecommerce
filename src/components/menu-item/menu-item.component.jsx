77b66caba8367627c511d274c510d23e924b5c89
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-22 19:49:14 +0530

    directory and menu item components added

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./menu-item.styles.scss";
+
+const MenuItem = ({title,imageUrl,size}) => {
+    return (
+      <div className={`${size} menu-item`}>
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        >
+        </div>
+        <div className="content">
+          <h1 className="title">{title.toUpperCase()}</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    );
+};
+
+export default MenuItem;
