d41b1e42d69e57ba347e14905c809185e1c2f734
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-05-24 02:18:48 +0530

    header component with styles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch,Route } from 'react-router-dom';
 import './App.css';
+import Header from "./components/header/header.component";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./header.styles.scss";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <Link className='logo-container' to="/">
+                <Logo className="logo" />
+            </Link>
+            <div className="options">
+                <Link className="option" to='/shop'>
+                    SHOP
+                </Link>
+                <Link className="option" to='/shop'>
+                    CONTACT
+                </Link>
+            </div>
+        </div>
+    )
+}
+        
+export default Header;
