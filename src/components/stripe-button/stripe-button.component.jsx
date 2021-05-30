2b2ddab31476dea6452345c83ab7a557fb8b1a80
Roshan Kanwar <rkanwar0708@gmail.com>
2021-05-31 02:48:05 +0530

    updated .env.example file

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_51GqdsWJ5kGLDsVifD3181kTYJB5DAjIB4pi4bYzDwVK24BeBpuXaAKvlksPb77qbYZGx2oumD04WEs7jdI04tBoZ00HWdBOBFM';
+    const publishableKey = process.env.REACT_APP_PUBLISHABLE_KEY;
 
     const onToken = token => {
         console.log(token);
